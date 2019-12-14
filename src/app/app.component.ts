ea2ebcdde07e33b74ef71ab8a0347038c1e0e43e
GabrielGCamata <gabrielcamata150@hotmail.com>
2019-12-14 10:43:32 -0300

    subindo projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PhotoService } from './photos/photo/photo.service';
+import { Photo } from './photos/photo/photo';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+}
